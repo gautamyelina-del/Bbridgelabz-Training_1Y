fb1ddbfebf5ae6aa44ae498630b613a68139f143
gautamyelina-del <gautamyelina@gmail.com>
2026-02-17 10:14:18 +0530

    [Yelinna]Level 1 arrays added

---
+++ Arrays/ClassWork/fizzbuzz.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Fizzbuzz
+{
+	public static void main(String[] args){
+    System.out.println("Enter the number")
+	int num=20;
+	String result[]=new String[20]
+	for(i=0;i<num;j++)
+		if(num%3==0&&num%5==0);
+		{
+			
+		    result[i]="Fizzbuzz";
+		}
+		else if (i%3==0)
+		{
+		    result[i]=="Fizz";
+		}
+		else if(i%5==0)
+		{
+			result[i]=:Buzz";
+		}
+		
+			
+		
+		
