f606ce8f9f69fc2675c52d70559ac120aec6db87
gautamyelina-del <gautamyelina@gmail.com>
2026-02-19 12:28:18 +0530

    [Yelina] level 1 added

---
+++ AdvanceTopic/src/com/gla/Abstraction/Runner.java
@@ -0,0 +1,13 @@
+package com.gla.Abstraction;
+
+public class Runner {
+    public static void main(String[] args){
+        Student S1 = new Student();
+        S1.position();
+        S1.info();
+        System.out.println("____________________________________________________________");
+        Teacher T1=new Student();
+        T1.position();
+        T1.info();
+    }
+}
---
+++ AdvanceTopic/src/com/gla/Abstraction/Student.java
@@ -0,0 +1,10 @@
+package com.gla.Abstraction;
+
+public class Student extends Teacher
+{
+    @Override
+    public void info(){
+        System.out.println("My name is Yelina");
+        System.out.println("I am from Delhi");
+    }
+}
